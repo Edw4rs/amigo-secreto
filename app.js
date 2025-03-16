f65b503715fe8f9ce0b09303e0c87f3eb95a3f59
Edwars Barahona <edwarsbarahona12@gmail.com>
2025-03-15 23:52:54 -0500

    Actualizar el archivo index y js

--- app.js
+++ app.js
@@ -1,60 +1,61 @@
-//creamos un array para almacenar los nombres 
-let amigos = [];
+//1. Crear un array para almecenar los nombres 
+let listAmigos = [];
 
-//creamos la funcion para agregar amigos
 function agregarAmigo() {
-    //captura el valor de entrada
-    const input = document.getElementById("amigo");
+    //captura el valor de entrada 
+    const input= document.getElementById("amigo");
     const nombre = input.value.trim();
 
-    //condicional para validar la entrada
+    //valida la entrada
     if (nombre ==="") {
         alert("Por favor, ingresa un nombre valido.");
         return;
     }
-
-    //actualizar el array de amigos
+    
+    //actualiza el array de amigos
     listAmigos.push(nombre);
 
     //muestra la lista de amigos en la pagina 
     actualizarLista();
 
-    //limpia el campo de la entrada
-    input.value="";  
+    //limpia el campo de entrada 
+    input.value="";
 }
 
- //creamos la funcion para optener el elemento de la lista HTML
 function actualizarLista() {
-    let lista = document.createElement("li");
+    //Obtener el elemento de la lista en el HTML
+    let lista = document.getElementById("listaAmigos");
 
-    //limpiar la lista existente para evitar duplicados
+    //limpiar la lista exitente para evitar duplicados
     lista.innerHTML = "";
 
-    //iteriar sobre el arreglos y crear elementos <li> para cada amigo
-    //usamos el ciclo for
+    //iterar sobre el arreglo 3y crear elemnetos <li> para cada amigo
     for (let amigo of listAmigos) {
         let li = document.createElement("li");
         li.textContent = amigo;
-        lista.appendChild(li)//agrgar el <li> a la lista 
+        lista.appendChild(li)//agrgar el <li> a la lista
+        
     }
+    
 }
 
-//obtener el elemento donde se mostrara el resultado
 function sortearAmigo() {
+    //obtener el elemento donde se mostrara el resultado
     let resultado = document.getElementById("resultado");
-    
-    //validar que haya amigos en la lista antes de sortear
+
+    //validar que haya amigos en la lista antes de sortear 
     if (listAmigos.length === 0) {
-        alert("No hay amigos en la laista para sortear. ");
+        alert("No hay amigos en la lista para sortear.")
         return;
     }
 
-    //generar indice aleatorio
+    //generar inidice aleatorio
     let indiceAleatorio = Math.floor(Math.random()*listAmigos.length);
 
     //obtener el nombre del amigo sorteado
     let amigoSorteado = listAmigos[indiceAleatorio];
 
-    //mostrar el resultado en la pagina 
+    //mostrar el resultado en la pagina
     resultado.innerHTML = `<li>${amigoSorteado} es el amigo secreto 🎉</li>`;
-}
+    
+} 
